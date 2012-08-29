f9c034f2c7b4d31e5d39ac871e05879e353d0d16
Matthew Soldo <matt@soldo.org>
2012-08-29 14:50:04 -0700

    Change explicit sequence creation to use serial

--- ddl.sql
+++ ddl.sql
@@ -1,39 +1,21 @@
 CREATE TABLE products (
-    id integer NOT NULL,
+    id serial,
     title character varying(255),
     description text,
     price numeric(10,2)
 );
 
-CREATE SEQUENCE products_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-ALTER SEQUENCE products_id_seq OWNED BY products.id;
-
 CREATE TABLE purchases (
-    id integer NOT NULL,
+    id serial,
     user_id integer,
     items decimal(10,2) [100][1],
 	occurred_at timestamp
 );
 
-CREATE SEQUENCE purchases_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-ALTER SEQUENCE purchases_id_seq OWNED BY purchases.id;
-
 CREATE EXTENSION hstore;
 
 CREATE TABLE users (
-    id integer NOT NULL,
+    id serial,
     first_name character varying(50),
     last_name character varying(50),
     email character varying(255),
@@ -43,25 +25,15 @@ CREATE TABLE users (
     last_login timestamp without time zone
 );
 
-CREATE SEQUENCE users_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER SEQUENCE users_id_seq OWNED BY users.id;
-
-COPY products (id, title, description, price) FROM stdin;
-1	Django Pony	The Django Pony	50.00
-2	Pink Pony	A Pink Pony	299.99
-3	Ruby Gem	All the Rubies!	49.50
-4	Purple Gem	Its purple...	9.99
-5	Cooking 101	Cookbook for getting started	9.99
-6	Cooking 102	Cookbook for getting started	15.00
-7	Cooking 401	Cookbook for getting started	29.99
-8	Heroku Dyno	A single process on heroku	36.00
+COPY products (title, description, price) FROM stdin;
+Django Pony	The Django Pony	50.00
+Pink Pony	A Pink Pony	299.99
+Ruby Gem	All the Rubies!	49.50
+Purple Gem	Its purple...	9.99
+Cooking 101	Cookbook for getting started	9.99
+Cooking 102	Cookbook for getting started	15.00
+Cooking 401	Cookbook for getting started	29.99
+Heroku Dyno	A single process on heroku	36.00
 \.
 
 CREATE OR REPLACE FUNCTION total(decimal(10,2)[][]) RETURNS decimal(10,2) AS $$
